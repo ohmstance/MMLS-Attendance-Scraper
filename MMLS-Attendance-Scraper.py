aa35b13fd44925d194e4c21674cd6cf72c1d8195
ohmstance <58171737+ohmstance@users.noreply.github.com>
2020-05-02 01:40:09 +0800

    Search algorithm change and
    The search algorithm in the dateToTimetableID function has changed to a conventional binary search. Simpler logic speeds up timetable_id searches. Also if no timetable_id is associated to input date, the search range is narrowed until a timetable_id is found. Additionally, SoupStrainer module is added and is used with BeautifulSoup to parse only 'input' tags.

--- MMLS-Attendance-Scraper.py
+++ MMLS-Attendance-Scraper.py
@@ -1,17 +1,19 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError, URLError
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 from sys import exit as exit
-from datetime import date
+from datetime import date, timedelta
 import concurrent.futures
 import time
+maxTimetableID = 99999
+minTimetableID = 1
 RETRIES = 3
 
 #made by munchbit
 def fetchHTML(timetableID): #Accepts timetable_id. Downloads attendance HTML bearing the timetable_id. If successful, returns BeautifulSoup HTML object, but returns None type otherwise.
     for x in range(RETRIES):
         try:
-            html = BeautifulSoup(urlopen('https://mmls.mmu.edu.my/attendance:0:0:'+str(timetableID), timeout=30), 'html.parser') #Apparently subjectID and coordinatorID doesn't matter for attendance links
+            html = BeautifulSoup(urlopen('https://mmls.mmu.edu.my/attendance:0:0:'+str(timetableID), timeout=30), 'html.parser', parse_only = SoupStrainer("input")) #Apparently subjectID and coordinatorID doesn't matter for attendance links
             return html
         except HTTPError as error:
             if error.code == 500:
@@ -22,56 +24,34 @@ def fetchHTML(timetableID): #Accepts timetable_id. Downloads attendance HTML bea
             continue
     exit("Network error. Try raising number of retries or obtain better network condition.")
 
-def dateToTimetableID(date, option): #Accepts date and option to choose either returning the first occurence or the last. Returns corresponding timetable_id satisfying the option selected.
-    MIN_TimetableID = 1
-    MAX_TimetableID = 99999
-    currTimetableID = -(-(MAX_TimetableID+MIN_TimetableID)//2)
-    step = -(-(MAX_TimetableID+MIN_TimetableID)//2)
-    previousAction = 0 #0: No action; -1: Stepped backwards; 1:  Stepped forwards.
+def dateToTimetableID(date, option): #Option: 1 for first occurence, -1 for last occurence; Binary search algorithm; Returns None if no classes on that date.
+    upperbound = maxTimetableID
+    lowerbound = minTimetableID
     while(True):
-        if currTimetableID < MIN_TimetableID:
-            step -= MIN_TimetableID - currTimetableID
-            currTimetableID = MIN_TimetableID
-        if currTimetableID > MAX_TimetableID:
-            step -= currTimetableID - MAX_TimetableID
-            currTimetableID = MAX_TimetableID
-
+        currTimetableID = (upperbound+lowerbound)//2
         html = fetchHTML(currTimetableID)
         if html is None:
-            if previousAction != -1:
-                if step == 1: return None
-                step = -(-step)//2
-            currTimetableID -= step
-            previousAction = -1
+            upperbound = currTimetableID - 1
             continue
-
         currDate = date.fromisoformat(html.find('input', id="class_date")['value'])
-        if (date-currDate).days < 0: #Desired date falls behind current date
-            if previousAction != -1:
-                if step == 1: return None #Terminates seek when timetable_id for the date does not exist
-                step = -(-step)//2
-            currTimetableID -= step
-            previousAction = -1
-        elif (date-currDate).days > 0: #Desired date is ahead of current date
-            if previousAction != 1:
-                if step == 1: return None
-                step = -(-step)//2
-            currTimetableID += step
-            previousAction = 1
-        elif option == "first":
-            html = fetchHTML(currTimetableID-1)
-            if html is None: return currTimetableID
-            elif date != date.fromisoformat(fetchHTML(currTimetableID-1).find('input', id="class_date")['value']): return currTimetableID
-            elif previousAction != -1: step = -(-step)//2
-            currTimetableID -= step
-            previousAction = -1
-        elif option == "last":
-            html = fetchHTML(currTimetableID+1)
-            if html is None: return currTimetableID #handle error 500 in next timetable_id
-            elif date != date.fromisoformat(html.find('input', id="class_date")['value']): return currTimetableID #if next timetable_id's day is different
-            elif previousAction != 1: step = -(-step)//2 #continues here if next timetable_id's day is the same
-            currTimetableID += step
-            previousAction = 1
+        if (date - currDate).days > 0:
+            lowerbound = currTimetableID + 1
+        elif (date - currDate).days < 0:
+            upperbound = currTimetableID - 1
+        elif (date - currDate).days == 0 and option == 1:
+            html = fetchHTML(currTimetableID - 1)
+            if date.fromisoformat(html.find('input', id="class_date")['value']) != currDate:
+                return currTimetableID
+            upperbound = currTimetableID - 1
+        elif (date - currDate).days == 0 and option == -1:
+            html = fetchHTML(currTimetableID + 1)
+            if html is None:
+                return currTimetableID
+            if date.fromisoformat(html.find('input', id="class_date")['value']) != currDate:
+                return currTimetableID
+            lowerbound = currTimetableID + 1
+        if upperbound < lowerbound:
+            return None
 
 def askYesNo(question): #Accepts string -- preferably a question. Ask the user if yes, or no. Returns boolean result where y: True and n: False.
     while True:
@@ -104,12 +84,21 @@ def main():
     startTime = time.time()
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
-        startTimetableID = executor.submit(dateToTimetableID, startDate, "first")
-        endTimetableID = executor.submit(dateToTimetableID, endDate, "last")
-        startTimetableID = startTimetableID.result()
-        endTimetableID = endTimetableID.result()
-        print("\nRange of timetable_id found. {}s elapsed.".format(time.time()-startTime))
-        print("Scraping started.")
+        while(True):
+            startTimetableID = executor.submit(dateToTimetableID, startDate, 1)
+            endTimetableID = executor.submit(dateToTimetableID, endDate, -1)
+            startTimetableID = startTimetableID.result()
+            endTimetableID = endTimetableID.result()
+            if startTimetableID is None:
+                startDate += timedelta(days=1)
+            if endTimetableID is None:
+                endDate -= timedelta(days=1)
+            if startDate > endDate:
+                print("No classes exist in range.")
+            if startTimetableID is None or endTimetableID is None:
+                continue
+            print("\nSearching from {} to {}. Range found in {}s.".format(startDate.isoformat(), endDate.isoformat(), time.time()-startTime))
+            break
 
         futures = [executor.submit(fetchHTML, startTimetableID+x) for x in range(endTimetableID-startTimetableID+1)]
         while len(futures) > 0: #For as long as there are any futures, result of futures are parsed in order it is submitted. Once done, the queue (list of futures) is popped and the elements are shifted forward. All elements are accessible sequentially in the first element as queue is popped.
